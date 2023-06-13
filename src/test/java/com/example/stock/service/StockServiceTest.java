28323d1b1b6f1ade18652f8431b9e3555a314a21
lay423 <junpang486@gmail.com>
2023-06-13 14:08:58 +0900

    feat: 문제점

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,5 +40,27 @@ class StockServiceTest {
 
     Stock stock = stockRepository.findById(1L).orElseThrow();
     assertEquals(99, stock.getQuantity());
+  }
+
+  @Test
+  public void 동시에_100개의_요청() throws InterruptedException {
+    int threadCount = 100;
+    ExecutorService executorService = Executors.newFixedThreadPool(32);
+    CountDownLatch latch = new CountDownLatch(threadCount);
+    for (int i = 0; i < threadCount; i++) {
+      executorService.submit(() -> {
+        try {
+          stockService.decrease(1L, 1L);
+        } finally {
+          latch.countDown();
+        }
+      });
+    }//for
+
+    latch.await();
+    Stock stock = stockRepository.findById(1L).orElseThrow();
+
+    // 100 - (1 * 100) = 0
+    assertEquals(0L, stock.getQuantity());
   }
 }
